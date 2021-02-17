de389e39799251868d078d462d2dc2f7d5eac3d6
Gaurav Raghav <78542102+gauravraghav1988@users.noreply.github.com>
2021-02-17 14:23:28 +0530

    Add files via upload

---
+++ starts printing from left.py
@@ -0,0 +1,12 @@
+
+# it will print stars for any no of floors from left
+n=int(input("Enter no of floors to print of *:\n"))
+for i in range(1,n+1):
+    stars='*'*i
+    print(stars)
+
+'''result for n=3 will be :
+*
+**
+***
+'''
